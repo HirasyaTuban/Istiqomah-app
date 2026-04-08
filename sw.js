4a109a96766011bc50b48d81326e2ab54e782698
HirasyaTuban <144972421+HirasyaTuban@users.noreply.github.com>
2026-04-08 20:17:19 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'istiqomah-static-b9i';
-const DYNAMIC_CACHE = 'istiqomah-dynamic-b9i';
+const STATIC_CACHE = 'istiqomah-static-c2i';
+const DYNAMIC_CACHE = 'istiqomah-dynamic-c2i';
 
 // Assets to cache on install
 const STATIC_ASSETS = [
